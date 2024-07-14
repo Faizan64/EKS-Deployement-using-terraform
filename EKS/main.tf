607a2f136867ae2b485abf310b9fd02b99af44db
Faizan <91891601+Faizan64@users.noreply.github.com>
2024-07-14 08:07:11 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -52,3 +52,10 @@ module "eks" {
     Terraform   = "true"
   }
 }
+data "aws_eks_cluster" "cluster"{
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster1_auth" "cluster"{
+  name = module.eks.cluster_id
+}
